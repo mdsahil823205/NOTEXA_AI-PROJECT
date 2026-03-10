ad10d456f940e66e5d746c2c93b9932327f6a692
MD SAHIL <165897893+mdsahil823205@users.noreply.github.com>
2026-03-10 16:21:41 +0530

    Refactor getToken function for better readability

--- server/src/utils/token.js
+++ server/src/utils/token.js
@@ -1,13 +1,17 @@
 const jwt = require("jsonwebtoken");
- const getToken = (userid) => {
+
+const getToken = (userid) => {
   try {
-    const token = jwt.sign({ userid }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
-    console.log("Generated Token:", token);
+    const token = jwt.sign(
+      { userid },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
     return token;
   } catch (err) {
     console.error("Error generating token:", err);
   }
 };
-module.exports={ getToken }
+
+module.exports = { getToken };
