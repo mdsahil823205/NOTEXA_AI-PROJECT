bc277a23e3f3e80227e84fe780cb6a65774c7b57
MD SAHIL <165897893+mdsahil823205@users.noreply.github.com>
2026-03-10 16:20:59 +0530

    Refactor isAuthenticated middleware for error handling

--- server/src/middleware/user.middleware.js
+++ server/src/middleware/user.middleware.js
@@ -1,25 +1,33 @@
 const jwt = require("jsonwebtoken");
+
 const isAuthenticated = (req, res, next) => {
-    try {
-        const token = req.cookies?.token;
-        if (!token) {
-            return res.status(401).json({
-                message: "tooken is not provided"
-            })
-        }
-        const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-        if (!verifyToken) {
-            return res.status(401).json({
-                message: "Invalid token"
-            })
-        }
-        req.userId = verifyToken.userId || verifyToken.userid || verifyToken.id;
-        next();
-    } catch (error) {
-        console.log("this error come from isAuthenticated middleware:", error);
-        return res.status(500).json({
-            message: "Internal server error"
-        })
+  try {
+    const token = req.cookies?.token;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Token not provided",
+      });
     }
-}
-module.exports = isAuthenticated
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({
+        message: "Invalid token",
+      });
+    }
+
+    req.userId = decoded.userid;
+
+    next();
+  } catch (error) {
+    console.log("Error in isAuthenticated middleware:", error);
+
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+};
+
+module.exports = isAuthenticated;
