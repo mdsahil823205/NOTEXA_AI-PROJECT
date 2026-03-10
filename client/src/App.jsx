2e3843c1c31079748b540c8d9062c9989472460d
MD SAHIL <165897893+mdsahil823205@users.noreply.github.com>
2026-03-10 16:05:31 +0530

    Refactor user data fetching and loading state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ReactLenis } from "lenis/react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
-import { fetchCurrentUserapi } from "./services/api";
-import { useDispatch, useSelector } from "react-redux";
 import Pricing from "./pages/Pricing";
 import History from "./pages/History";
 import Notes from "./pages/Notes";
+
+import { fetchCurrentUserapi } from "./services/api";
+
 export const serverUrl = "https://notexa-ai-project-server.onrender.com";
+
 const App = () => {
   const dispatch = useDispatch();
+  const userData = useSelector((state) => state.user.userData);
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchCurrentUserapi(dispatch);
+    const getUser = async () => {
+      await fetchCurrentUserapi(dispatch);
+      setLoading(false);
+    };
+
+    getUser();
   }, [dispatch]);
-  const userData = useSelector((state) => state.user.userData);
-  console.log("Current user data:", userData);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen text-3xl w-full bg-linear-to-br from-[#050505]  via-[#0a0a1a]  to-[#000000]  text-white ">
+        Loading...
+      </div>
+    );
+  }
+
   return (
     <ReactLenis
       root
@@ -35,18 +54,22 @@ const App = () => {
           path="/"
           element={userData ? <Home /> : <Navigate to="/auth" replace />}
         />
+
         <Route
           path="/auth"
-          element={userData ? <Navigate to="/" replace /> : <Auth />}
+          element={!userData ? <Auth /> : <Navigate to="/" replace />}
         />
+
         <Route
           path="/history"
           element={userData ? <History /> : <Navigate to="/auth" replace />}
         />
+
         <Route
           path="/pricing"
           element={userData ? <Pricing /> : <Navigate to="/auth" replace />}
         />
+
         <Route
           path="/notes"
           element={userData ? <Notes /> : <Navigate to="/auth" replace />}
